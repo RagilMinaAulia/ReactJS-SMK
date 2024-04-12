93717c6d126aa1d6486d0e7597cb4043d1095ccf
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-04-12 20:04:26 +0700

    Komponen

---
+++ react-yt/src/pages/Sejarah.js
@@ -0,0 +1,10 @@
+function Sejarah() {
+    return (
+      <div className="App">
+          <h1>Sejarah SMKN 2 Buduran</h1>
+          <p>Isi</p>
+      </div>
+    );
+  }
+  
+  export default Sejarah;
---
+++ react-yt/src/pages/Tentang.js
@@ -0,0 +1,10 @@
+function Tentang() {
+    return (
+      <div className="App">
+          <h1>Tentang SMKN 2 Buduran</h1>
+          <p>Isi tentang</p>
+      </div>
+    );
+  }
+  
+  export default Tentang;
