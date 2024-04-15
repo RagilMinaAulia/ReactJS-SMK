4d68a406b37c1772f1e105b8fefd7935b9d8134c
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-04-15 20:20:58 +0700

    State

--- react-yt/src/pages/Tentang.js
+++ react-yt/src/pages/Tentang.js
@@ -1,9 +1,23 @@
+import {useState} from 'react';
+
 function Tentang() {
-    return (
+  const [count, setCount] = useState(0);
+  function tambah() {
+    setCount(count+1)
+  }
+
+  function kurang() {
+    if (count > 0) {
+      setCount(count-1)
+    }
+  }
+
+  return (
       <div className="App">
-          <h1>Tentang SMKN 2 Buduran</h1>
+          <h1>Tentang SMKN 2 Buduran counter: {count}</h1>
           <p>Isi tentang</p>
-          <button type="button" className="btn btn-dark">Dark</button>
+          <button type="button" onClick={tambah} className="btn btn-dark">Tambah</button>
+          <button type="button" onClick={kurang} className="btn btn-secondary">Kurang</button>
       </div>
     );
   }
