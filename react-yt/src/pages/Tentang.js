00fdb9bd4d140ffa292baf0242e39ba98848ddd4
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-04-15 19:25:49 +0700

    Menambahkan Bootstrap

--- react-yt/src/pages/Tentang.js
+++ react-yt/src/pages/Tentang.js
@@ -3,6 +3,7 @@ function Tentang() {
       <div className="App">
           <h1>Tentang SMKN 2 Buduran</h1>
           <p>Isi tentang</p>
+          <button type="button" class="btn btn-dark">Dark</button>
       </div>
     );
   }
